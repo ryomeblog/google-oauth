2f8a2852d32b2e0beb6e293303d720e3abce0f0f
ryomeblog <ryome.blog@gmail.com>
2023-05-17 08:36:40 +0900

    適切なコメントを追加

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,35 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const cors = require('cors');
-const qs = require('qs');
-const axios = require('axios');
+// 必要なモジュールをインポート
+const serverless = require('serverless-http'); // serverless-httpモジュールを利用することで、AWS Lambdaと連携しやすくなる
+const express = require('express'); // Express.jsを利用してAPIサーバーを構築
+const cors = require('cors'); // CORS制御を実現するためのモジュール
+const qs = require('qs'); // クエリストリングを操作するためのモジュール
+const axios = require('axios'); // HTTPリクエストを簡単に行うためのライブラリ
 
 const app = express();
 
-// CORSオプション
+// CORSの設定
 const corsOptions = {
-  origin: process.env.FRONT_URL,
-  credentials: true
+  origin: process.env.FRONT_URL, // フロントエンドのURLを環境変数から取得
+  credentials: true // クレデンシャルを含むリクエストを許可する
 };
 
-// CORS設定を適用
+// CORS設定とJSONパーサーをミドルウェアとして適用
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Google認証用のエンドポイント
 app.post('/api/v1/auth/google', async (req, res) => {
   const { code } = req.body;
 
+  // codeがリクエストに含まれていない場合は400エラーを返す
   if (code === undefined) {
     return res.status(400).json({ message: 'Invalid request' });
   }
 
   let accessToken = '';
   let refreshToken = '';
+
+  // Google OAuthの認証情報を設定
   const params = {
     code: code,
     client_id: process.env.GOOGLE_CLIENT_ID,
@@ -33,6 +38,7 @@ app.post('/api/v1/auth/google', async (req, res) => {
     grant_type: 'authorization_code'
   }
 
+  // Googleにアクセストークンをリクエスト
   try {
     const response = await axios({
       method: 'post',
@@ -51,6 +57,8 @@ app.post('/api/v1/auth/google', async (req, res) => {
   }
 
   let user = {};
+
+  // 取得したアクセストークンを利用してユーザー情報を取得
   try {
     const response = await axios({
       method: 'get',
@@ -66,13 +74,14 @@ app.post('/api/v1/auth/google', async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 
+  // ユーザー情報とトークンをクライアントに返す
   return res.status(200).json({
-    user: user,
-    access_token: accessToken,
-    refresht_oken: refreshToken,
-    status: 200
+    user: user, // ユーザ情報
+    access_token: accessToken, // アクセストークン
+    refresh_token: refreshToken, // リフレッシュトークン
+    status: 200 // ステータスコード
   });
 });
 
-
+// サーバーレスアプリケーションとしてエクスポート
 module.exports.handler = serverless(app);
--- frontend/sample-oauth/src/App.js
+++ frontend/sample-oauth/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import GoogleButton from 'react-google-button'
-import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+// 必要なモジュールとコンポーネントをインポート
+import React, { useState } from 'react'; // ReactとuseStateフックをインポート
+import axios from 'axios'; // HTTPリクエストを簡単に行うためのライブラリ
+import GoogleButton from 'react-google-button'; // Googleのログインボタンコンポーネント
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google"; // Google認証関連のProviderとHook
 
 const GoogleAuthComponent = () => {
+  // ステートの初期化
   const [id, setId] = useState('');
   const [email, setEmail] = useState('');
   const [givenName, setGivenName] = useState('');
@@ -12,23 +14,27 @@ const GoogleAuthComponent = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const GoogleAuthComponentLogin = () => {
+    // Googleログインの設定
     const login = useGoogleLogin({
-      onSuccess: (codeResponse) => responseGoogle(codeResponse),
+      onSuccess: (codeResponse) => responseGoogle(codeResponse), // ログイン成功時の処理
       flow: "auth-code",
       scope: "email profile openid",
     });
 
+    // ログイン成功時に実行される関数
     const responseGoogle = (codeResponse) => {
       console.log(JSON.stringify(codeResponse));
+      // サーバーへアクセストークンをリクエスト
       axios.post(`${process.env.REACT_APP_API_URL}/api/v1/auth/google`, { code: codeResponse.code })
         .then(response => {
           console.log(response);
+          // ステートを更新
           setId(response.data.user.id);
           setEmail(response.data.user.email);
           setGivenName(response.data.user.given_name);
           setAccessToken(response.data.access_token);
           setRefreshToken(response.data.refresht_oken);
-          setIsLoggedIn(true);
+          setIsLoggedIn(true); // ログイン状態をtrueに設定
         })
         .catch(error => {
           console.error('Error occurred:', error);
@@ -38,10 +44,12 @@ const GoogleAuthComponent = () => {
     return (
       <>
         {!isLoggedIn ? (
+          // ログインボタンを表示
           <GoogleButton
-            onClick={() => { login(); }}
+            onClick={() => { login(); }} // ボタンクリックでログイン処理を実行
           />
         ) : (
+          // ログイン後の情報とログアウトボタンを表示
           <div>
             <p>Id: {id}</p>
             <p>Email: {email}</p>
@@ -54,6 +62,8 @@ const GoogleAuthComponent = () => {
       </>
     );
   }
+
+  // Google認証のProviderを設定してコンポーネントを返す
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <GoogleAuthComponentLogin />
@@ -61,4 +71,5 @@ const GoogleAuthComponent = () => {
   );
 }
 
+// GoogleAuthComponentをエクスポート
 export default GoogleAuthComponent;
